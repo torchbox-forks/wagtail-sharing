0290430c6b8015b619ba8f245030c268b1971250
Tom Readings <tom.readings@torchbox.com>
2020-07-30 16:36:02 +0100

    Generate token based sharing link in wagtail admin

--- wagtailsharing/urls.py
+++ wagtailsharing/urls.py
@@ -7,9 +7,10 @@ from wagtail.urls import urlpatterns as wagtailcore_urlpatterns
 from wagtailsharing.views import ServeView, TokenServeView
 
 
-if getattr(settings, "WAGTAILSHARING_TOKENIZE_URL", True):
+if getattr(settings, "WAGTAILSHARING_TOKENIZE_URL", False):
+    share_path = getattr(settings, "WAGTAILSHARING_TOKEN_SHARE_PATH", "share")
     wagtailsharing_serve_path = re_path(
-        r"^share/([\w\.\-\_]+)/$",
+        rf"^{share_path}/([\w\.\-\_]+)/$",
         TokenServeView.as_view(),
         name="wagtail_serve",
     )
